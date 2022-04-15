c1a2069c1d37c2069cb93f653dd7af66a39e7414
yusufhadi <hadiyusuf889@gmail.com>
2022-04-15 15:06:34 +0800

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,8 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="sase express, PT. Sarana Anugerah Semesta, Sarana Anugerah Semesta">
+    <meta name="description" content="Selamat Datang di Website Resmi Sase Express">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
